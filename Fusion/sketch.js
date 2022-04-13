dba75c0dfb7d8a55e6fa4b84b50e9acb6cdd0e13
IJslap <45030475+ijslapje@users.noreply.github.com>
2022-04-13 13:56:47 +0200

    Skelecton tracking demo

--- Fusion/sketch.js
+++ Fusion/sketch.js
@@ -12,7 +12,6 @@ function setup() {
 }
 
 function gotPoses(poses) {
-  console.log(poses);
   if (poses.length > 0) {
     pose = poses[0].pose;
     skeleton = poses[0].skeleton;
@@ -25,28 +24,16 @@ function modelLoaded() {
 
 
 function draw() {
-  background(0);
+  image(video, 0, 0);
 
   if (pose) {
-    let eyeR = pose.rightEye;
-    let eyeL = pose.leftEye;
-    let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
-    fill(255, 0, 0);
-    ellipse(pose.nose.x, pose.nose.y, d * 4.5);
-
     for (let i = 5; i < pose.keypoints.length; i++) {
       let x = pose.keypoints[i].position.x;
       let y = pose.keypoints[i].position.y;
+
+      
       fill(0, 255, 0);
-    }
-
-    for (let i = 0; i < skeleton.length; i++) {
-      let a = skeleton[i][0];
-      let b = skeleton[i][1];
-      strokeWeight(2);
-      stroke(255);
-
-      line(a.position.x, a.position.y, b.position.x, b.position.y);
+      ellipse(x, y, 16, 16);
     }
   }
 }
--- PaperJS/js/tutorialScript.js
+++ PaperJS/js/tutorialScript.js
@@ -1,35 +1,11 @@
-// import { greet, message } from "./utils.js";
+var path = new Path.Circle({
+	center: view.center,
+	radius: 70,
+	fillColor: 'red'
+});
 
-// const greet_scaler = greet("Scaler");
-
-// console.log(greet_scaler); // Hello, Scaler
-// console.log(message); // How you doing?
-
-
-var mousePoint = view.center;
-var amount = 25;
-var colors = ['red', 'white', 'blue', 'white'];
-
-for (var i = 0; i < amount; i++) {
-	var rect = new Rectangle([0, 0], [25, 25]);
-	rect.center = mousePoint;
-	var path = new Path.Rectangle(rect, 6);
-	path.fillColor = colors[i % 4];
-	var scale = (1 - i / amount) * 20;
-	path.scale(scale);
-}
-
-function onMouseMove(event) {
-	mousePoint = event.point;
-}
-
-var children = project.activeLayer.children;
 function onFrame(event) {
-	for (var i = 0, l = children.length; i < l; i++) {
-		var item = children[i];
-		var delta = (mousePoint - item.position) / (i + 5);
-		item.rotate(Math.sin((event.count + i) / 10) * 7);
-		if (delta.length > 0.1)
-			item.position += delta;
-	}
+	// Each frame, change the fill color of the path slightly by
+	// adding 1 to its hue:
+	path.fillColor.hue += 1;
 }
