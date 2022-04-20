e04c036d953e954482ed2697af6da6cd8ef57e6f
Ian Robeerst <ianrobeerst@livewallgroup.com>
2022-04-20 15:08:14 +0200

    Update sketch.js

--- Fusion/sketch.js
+++ Fusion/sketch.js
@@ -1,16 +1,14 @@
-import {pose} from './getJoints.js';
+import { pose } from './getJoints.js';
 
-	window.onload = function() {
-
-
-	}
-
-	export function drawSketch(){
-		if (typeof pose == 'undefined'){
-			console.log('GA IN DE CAMERA STAAN KUT!');
-		}
-		else{
-			console.log(pose.nose.x);
-		}
-	}
+window.onload = function () {
 	
+}
+
+export function drawSketch() {
+	if (typeof pose == 'undefined') {
+		console.log('GA IN DE CAMERA STAAN KUT!');
+	}
+	else {
+		//console.log(pose.nose.x);
+	}
+}
